788332a45e6187d3a2349f951c7dbd80da5d92b8
FoRoKo1o <marcin.pieta12211@gmail.com>
2025-01-26 14:33:40 +0100

    More testing

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-
 const puppeteer = require('puppeteer');
 const simpleGit = require('simple-git');
 const path = require('path');
 const fs = require('fs');
+const { exec } = require('child_process');
+
+// Ustawienie ścieżki do pliku logu
+const logFile = '/home/TryHackMeBadgeUpdate/log.txt';
+
+// Zapisz datę uruchomienia skryptu do logów
+fs.appendFileSync(logFile, `Skrypt uruchomiony: ${new Date()}\n`);
+
+// Funkcja do załadowania klucza SSH
+function loadSSHKey() {
+  return new Promise((resolve, reject) => {
+    exec('eval $(ssh-agent -s) && ssh-add /root/.ssh/test', (error, stdout, stderr) => {
+      if (error) {
+        reject(`Błąd podczas ładowania klucza SSH: ${stderr || error}`);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
 
 (async () => {
   const url = 'https://tryhackme.com/api/v2/badges/public-profile?userPublicId=3195715';
   const outputPath = 'thmBadge.png';
-  const repoPath = `/home/FoRoKo1o`;
+  const repoPath = '/home/FoRoKo1o';
   const commitMessage = 'Automatyczny commit: Zaktualizowano screenshot';
 
- console.log(`test`);
   try {
+    // Załaduj klucz SSH przed rozpoczęciem operacji Gita
+    console.log('Ładowanie klucza SSH...');
+    await loadSSHKey();
+    console.log('Klucz SSH załadowany.');
+
     // Uruchom Puppeteer
     const browser = await puppeteer.launch({
       headless: true,
       executablePath: '/usr/bin/chromium-browser',
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-software-rasterizer'],
-//      dumpio: true,
     });
 
     const page = await browser.newPage();
     console.log(`Otwieram stronę: ${url}`);
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
 
     // Zapisz zrzut ekranu
     await page.screenshot({
       path: outputPath,
       clip: {
-        x: 1,
-        y: 1,
-        width: 324,
-        height: 82,
+        x: 5,
+        y: 5,
+        width: 318,
+        height: 72,
       },
     });
     console.log(`Screenshot zapisany jako ${outputPath}`);
 
     await browser.close();
 
+    process.env.GIT_SSH_COMMAND = '/usr/bin/ssh -i /root/.ssh/test';
+    process.env.PATH = '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin';
+
+    // Skopiuj plik do repozytorium
     const destinationPath = path.join(repoPath, 'thmBadge.png');
     fs.copyFileSync(outputPath, destinationPath);
     console.log(`Zrzut ekranu skopiowany do repozytorium: ${destinationPath}`);
@@ -55,7 +81,9 @@ const fs = require('fs');
 
     // Wykonaj push
     console.log('Wykonywanie pusha...');
-    await git.push();
+
+    //await git.push();
+    await git.push('origin', 'main', { '--verbose': true });
 
     console.log('Push zakończony sukcesem!');
 
